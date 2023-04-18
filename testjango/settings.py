7d888202f7f4570c38782adcc1f6bde9691c8c5d
PunciaTail <120395814+PunciaTail@users.noreply.github.com>
2023-04-18 18:15:24 +0900

    Update settings.py

--- testjango/settings.py
+++ testjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ib1dnk*(4vqy-05lm95+$-00gtu8_t1056b2_wc@p*0uh00(fy'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
